d11f5b2ab9d8c4ebb67d9de0fd125bb349e3287b
Tekazamlaeticia <107771129+Tekazamlaeticia@users.noreply.github.com>
2022-09-09 22:07:55 +0200

    add new function generatePassword

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 
+
+
+
 function getRandomLower(){
    
     return String.fromCharCode(Math.floor(Math.random()*26) + 97);
@@ -28,3 +31,32 @@ function getRandomSymbol(){
     return symbols[i];
 }
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+
+generateEl.addEventListener('click', ()=>{
+    const length = +lengthEl.value 
+    const hasLower = lowercaseEl.checked 
+    const hasUpper = uppercaseEl.checked 
+    const hasNumber = numbersEl.checked 
+    const hasSymbol = symbolsEl.checked 
+
+    resultEl.innerText = generatePassword(length, hasUpper, hasLower,  hasNumber, hasSymbol)
+    
+})
+
+function generatePassword(length, upper, lower,  number, symbol){
+    let generatePassword = ''
+    const typesCount = upper+lower+number+symbol
+    const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item =>Object.values(item)[0])
+
+    if(typesCount===0){
+        return ''
+    }
+    //i=0 lenght=20, 4 
+   
